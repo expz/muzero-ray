bug fixes including using actions from experience in value target computation instead of sampling from network's output distribution

muzero/muzero_tf_model.py
@@ -319,7 +319,12 @@ class MuZeroTFModelV2(TFModelV2):
         
     def representation(self, obs_batch: TensorType) -> TensorType:
         """obs should be of shape (batch_size, 32*4, screen_x, screen_y)"""
-        return self.representation_net(obs_batch)
+        hidden_state = self.representation_net(obs_batch)
+        # See Appendix G.
+        s_max = tf.math.reduce_max(hidden_state)
+        s_min = tf.math.reduce_min(hidden_state)
+        hidden_state = (hidden_state - s_min) / (s_max - s_min)
+        return hidden_state
     
     def prediction(self, hidden_state: TensorType) -> Tuple[TensorType, TensorType]:
         """hidden_state should be of shape (batch_size,) + self.state_shape"""
@@ -329,4 +334,9 @@ class MuZeroTFModelV2(TFModelV2):
     def dynamics(self, hidden_state: TensorType, action_batch: TensorType) -> Tuple[TensorType, TensorType]:
         """action should be of shape (batch_size) + self.state_shape[:-1]"""
         action_t = self._encode_atari_actions(action_batch)
-        return self.dynamics_net((hidden_state, action_t))
+        new_hidden_state, reward = self.dynamics_net((hidden_state, action_t))
+        # See Appendix G.
+        s_max = tf.math.reduce_max(new_hidden_state)
+        s_min = tf.math.reduce_min(new_hidden_state)
+        new_hidden_state = (new_hidden_state - s_min) / (s_max - s_min)
+        return new_hidden_state, reward

muzero/muzero_tf_policy.py
@@ -67,7 +67,7 @@ class MuZeroLoss:
         self.mean_entropy = tf.identity(0.)
         # loop through time steps
         for policy_pred in policy_preds:
-            action_dist = dist_class(model.untransform(tf.identity(policy_pred)), model)
+            action_dist = dist_class(tf.identity(policy_pred), model)
             self.mean_entropy += tf.reduce_mean(action_dist.entropy())
         # once done with looping, convert it to a tensor
         policy_preds = tf.transpose(tf.convert_to_tensor(policy_preds), perm=(1, 0, 2))
@@ -83,9 +83,9 @@ class MuZeroLoss:
         #print('value preds:', value_preds.shape)
         #print('policy targets:', policy_targets.shape)
         #print('policy preds:', policy_preds.shape)
-        self.total_reward_loss = reward_loss_fn(model.transform(tf.identity(reward_targets)), reward_preds, model)
-        self.total_vf_loss = model.config['value_loss_weight'] * value_loss_fn(model.transform(tf.identity(value_targets)), value_preds, model)
-        self.total_policy_loss = policy_loss_fn(model.transform(tf.identity(policy_targets)), policy_preds)
+        self.total_reward_loss = reward_loss_fn(tf.identity(reward_targets), reward_preds, model)
+        self.total_vf_loss = model.config['value_loss_weight'] * value_loss_fn(tf.identity(value_targets), value_preds, model)
+        self.total_policy_loss = policy_loss_fn(tf.identity(policy_targets), policy_preds)
         #print('total reward:', self.total_reward_loss.shape)
         #print('total vf:', self.total_vf_loss.shape)
         #print('total_policy:', self.total_policy_loss.shape)
@@ -107,19 +107,21 @@ def mu_zero_loss(policy,
                  dist_class: type,  # e.g., ray.rllib.models.tf.tf_action_dist.Categorical
                  train_batch: Dict[str, TensorType]) -> TensorType:
     obs = train_batch[SampleBatch.CUR_OBS]
+    actions = train_batch[SampleBatch.ACTIONS]
+    #if len(list(actions.shape)) < 2:
+    #    actions = tf.convert_to_tensor([actions])
     
     reward_preds = []
     value_preds = []
     policy_preds = []
     hidden_state = model.representation(obs)
-    for _ in range(policy.loss_steps):
+    for i in range(policy.loss_steps):
         value, action_probs = model.prediction(hidden_state)
-        # TODO: check whether this is supposed to be the actions from the train batch
-        categorical = tfp.distributions.Categorical(probs=action_probs)
-        actions = categorical.sample()
         value_preds.append(value)
         policy_preds.append(action_probs)
-        hidden_state, reward = model.dynamics(hidden_state, actions)
+        # j is a workaround for rllib passing in malformed batch at initialization.
+        #j = min(i, actions.shape[1] - 1)
+        hidden_state, reward = model.dynamics(hidden_state, actions[:, i])
         hidden_state = scale_gradient(hidden_state, 0.5)
         reward_preds.append(reward)
 
@@ -181,7 +183,7 @@ def action_distribution_fn(
     internal state outputs (or empty list if N/A)
     """
     value, action_probs = model.forward_with_value(obs_batch, is_training)
-    value, action_probs = model.untransform(value), model.untransform(action_probs)
+    value = model.untransform(value)
     action_dist = rllib_tf_dist.Categorical(tf.convert_to_tensor(action_probs), model)
     actions, logp = policy.exploration.get_exploration_action(
         action_distribution=action_dist,
@@ -245,12 +247,12 @@ def mu_zero_postprocess(
         gamma_k /= policy.gamma
         value_target += t[k:N + k] * gamma_k
     value_target = value_target.astype(np.float32)
-    sample_batch[Postprocessing.VALUE_TARGETS] = value_target
+    sample_batch[Postprocessing.VALUE_TARGETS] = policy.model.transform(value_target)
     
-    def rollout(values, default=0):
+    def rollout(values):
         """Matrix of shape (N, loss_steps) """
         arr = np.array([
-            values if i == 0 else np.concatenate((values[i:], [default for _ in range(min(i, N))]))
+            values if i == 0 else np.concatenate((values[i:], [values[-1] for _ in range(min(i, N))]))
             for i in range(policy.loss_steps)
         ])
         if len(arr.shape) == 3:
@@ -258,11 +260,12 @@ def mu_zero_postprocess(
         else:
             return np.transpose(arr, axes=(1, 0))
     
-    # TODO: Is this supposed to be untransformed?
     action_dist_inputs = sample_batch[SampleBatch.ACTION_DIST_INPUTS]
-    sample_batch['rollout_values'] = rollout(value_target)
-    sample_batch['rollout_rewards'] = rollout(rewards)
-    sample_batch['rollout_policies'] = rollout(action_dist_inputs, default = [0] * len(action_dist_inputs[0]))
+    sample_batch[SampleBatch.ACTIONS] = rollout(sample_batch[SampleBatch.ACTIONS])
+    sample_batch['rollout_values'] = policy.model.transform(rollout(value_target))
+    sample_batch['rollout_rewards'] = policy.model.transform(rollout(rewards))
+    #sample_batch['rollout_policies'] = rollout(action_dist_inputs, default = [0] * len(action_dist_inputs[0]))
+    sample_batch['rollout_policies'] = rollout(action_dist_inputs)
 
     # Setting the weight to -1 makes the weight be set to the max weight
     if PRIO_WEIGHTS not in sample_batch:

muzero/trainer.py
@@ -1,7 +1,9 @@
 import collections
 import copy
 import datetime
+from muzero.muzero import ATARI_DEFAULT_CONFIG, BOARD_DEFAULT_CONFIG
 import os
+import pickle
 import tempfile
 from typing import Callable, Tuple
 
@@ -19,6 +21,7 @@ from ray.rllib.utils.actors import create_colocated
 from ray.tune import Trainable
 from ray.tune.logger import Logger, UnifiedLogger
 from ray.tune.registry import ENV_CREATOR, _global_registry
+from ray.tune.resources import Resources
 from ray.tune.result import DEFAULT_RESULTS_DIR
 from ray.util.iter import LocalIterator
 import tensorflow as tf
@@ -99,19 +102,22 @@ class MuZeroTrainer(Trainable):
         # Create a number of replay buffer actors.
         num_replay_buffer_shards = self.config["optimizer"]["num_replay_buffer_shards"]
 
+        n_channels = self.config['n_channels']
+        loss_steps = self.config['loss_steps']
+        action_count = self.env_creator({}).action_space.n
         tensor_spec = (
-            tf.TensorSpec((96, 96, 4), tf.float32, SampleBatch.CUR_OBS),
+            tf.TensorSpec((96, 96, n_channels), tf.float32, SampleBatch.CUR_OBS),
             tf.TensorSpec((1,), tf.int32, SampleBatch.EPS_ID),
             tf.TensorSpec((1,), tf.int32, SampleBatch.UNROLL_ID),
-            tf.TensorSpec((1,), tf.int32, SampleBatch.ACTIONS),
-            tf.TensorSpec((4,), tf.float32, SampleBatch.ACTION_DIST_INPUTS),
+            tf.TensorSpec((loss_steps,), tf.int32, SampleBatch.ACTIONS),
+            tf.TensorSpec((action_count,), tf.float32, SampleBatch.ACTION_DIST_INPUTS),
             tf.TensorSpec((1,), tf.float32, SampleBatch.ACTION_PROB),
             tf.TensorSpec((1,), tf.float32, SampleBatch.ACTION_LOGP),
             tf.TensorSpec((1,), tf.bool, SampleBatch.DONES),
             tf.TensorSpec((1,), tf.float32, SampleBatch.REWARDS),
-            tf.TensorSpec((5, 4), tf.float32, 'rollout_policies'),
-            tf.TensorSpec((5,), tf.float32, 'rollout_rewards'),
-            tf.TensorSpec((5,), tf.float32, 'rollout_values'),
+            tf.TensorSpec((loss_steps, action_count), tf.float32, 'rollout_policies'),
+            tf.TensorSpec((loss_steps,), tf.float32, 'rollout_rewards'),
+            tf.TensorSpec((loss_steps,), tf.float32, 'rollout_values'),
         )
 
         # There's no way to control where the ReplayActors are located in a multi-node setup, so
@@ -143,7 +149,7 @@ class MuZeroTrainer(Trainable):
         def update_prio_and_stats(item: Tuple[ActorHandle, dict, int]) -> None:
             actor, prio_dict, count = item
             #print('actor', type(actor), actor)
-            #print('prio_dict:', type(prio_dict), prio_dict)
+            print('prio_dict:', type(prio_dict), prio_dict)
             #print('count:', type(count), count)
             actor.update_priorities.remote(prio_dict)
             metrics = LocalIterator.get_metrics()
@@ -248,3 +254,64 @@ class MuZeroTrainer(Trainable):
 
     def step(self):
         return next(self._global_op)
+
+    def cleanup(self):
+        if hasattr(self, "workers"):
+            self.workers.stop()
+        if hasattr(self, "optimizer") and self.optimizer:
+            self.optimizer.stop()
+
+    def save_checkpoint(self, checkpoint_dir: str) -> str:
+        checkpoint_path = os.path.join(checkpoint_dir,
+                                       "checkpoint-{}".format(self.iteration))
+        pickle.dump(self.__getstate__(), open(checkpoint_path, "wb"))
+
+        return checkpoint_path
+
+    def load_checkpoint(self, checkpoint_path: str):
+        extra_data = pickle.load(open(checkpoint_path, "rb"))
+        self.__setstate__(extra_data)
+
+    def __getstate__(self) -> dict:
+        state = {}
+        if hasattr(self, "workers"):
+            state["worker"] = self.workers.local_worker().save()
+        if hasattr(self, "optimizer") and hasattr(self.optimizer, "save"):
+            state["optimizer"] = self.optimizer.save()
+        return state
+
+    def __setstate__(self, state: dict):
+        if "worker" in state:
+            self.workers.local_worker().restore(state["worker"])
+            remote_state = ray.put(state["worker"])
+            for r in self.workers.remote_workers():
+                r.restore.remote(remote_state)
+        if "optimizer" in state:
+            self.optimizer.restore(state["optimizer"])
+
+    @classmethod
+    def default_resource_request(
+            cls, config: dict) -> Resources:
+        cf = dict(cls._default_config(config.get('action_type', None)), **config)
+        num_workers = cf["num_workers"] + cf["evaluation_num_workers"]
+        # TODO(ekl): add custom resources here once tune supports them
+        return Resources(
+            cpu=cf["num_cpus_for_driver"],
+            gpu=cf["num_gpus"],
+            memory=cf["memory"],
+            object_store_memory=cf["object_store_memory"],
+            extra_cpu=cf["num_cpus_per_worker"] * num_workers,
+            extra_gpu=cf["num_gpus_per_worker"] * num_workers,
+            extra_memory=cf["memory_per_worker"] * num_workers,
+            extra_object_store_memory=cf["object_store_memory_per_worker"] *
+            num_workers)
+
+    @classmethod
+    def _default_config(cls, action_type):
+        if action_type == 'atari' or action_type is None:
+            return ATARI_DEFAULT_CONFIG
+        elif action_type == 'board':
+            return BOARD_DEFAULT_CONFIG
+        else:
+            raise Exception(f"Unknown action type: {action_type}")
+        